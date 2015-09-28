203371dd01ba1d79ff9e61687c73c6fa54639911
Daniel Bogachek <bogachek.daniel@gmail.com>
2015-09-28 18:42:12 -0400

    initial documentation

---
+++ README.md
@@ -0,0 +1,4 @@
+twpiscanner
+Terrapin Works (Raspberry) Pi Scanner
+
+The goal of this project is to create an affordable and resource-light 3D scanner system. The current prototype features a Raspberry Pi, the Pi Camera, and some simple hardware.
--- scan.py
+++ scan.py
@@ -1,5 +1,87 @@
 #!/usr/bin/env python
 
+'''
+
+This code is for the TW Pi Scanner.
+
+Given pulses of .01 seconds, this stepper motor has 200 steps, hence 40 steps advancement * 5 = full revolution.
+
+Upon the user's press of the input button, the stage rotates and takes a picture for a total of 5 times
+to complete a single revolution. The 5 images are saved in the directory provided and are named after said
+directory.
+
+GPIO 23 (step) on the Pi is the step control on the A9488 stepper driver
+GPIO 18 (dir) on the Pi is the direction control on the A9488 stepper driver
+GPIO 24 (button) on the Pi is the tactile switch input
+
+
+Hardware Connections
+
+
+
+//////////////////// 5V Regulator
+
+VIN <--> VCC (12V and abvove)
+GND <--> GND
+VOUT <--> RPi 5V
+
+////////////////////
+
+
+
+
+//////////////////// RPi
+
+step <--> A9488 STEP
+dir <--> A9488 DIR
+button <--> tactile switch output, parallel with 10K pull-down to GND
+GND <--> GND
+5V <--> VOUT
+3.3V <--> A9488 VDD
+
+////////////////////
+
+
+
+
+//////////////////// A9488 Stepper Driver
+
+IMPORTANT FOR HARDWARE SETUP: Current Limit = VREF × 2.5
+Value set at .4V so that current limit is 1A
+-
+VMOT <--> VCC
+GND <--> GND
+VDD <--> VDD 3.3V from RPi - RPi IO is 3.3V and VDD on the stepper driver uses VDD as logic reference
+GND <--> GND
+-
+Connect RST & SLP
+STEP <--> RPi step
+DIR <--> RPi dir
+-
+1A,1B - first stepper coil
+2A,2B - second stepper coil
+
+////////////////////
+
+
+
+
+//////////////////// Tactile Switch
+
+tactile switch input <--> 3.3V
+tactile switch output <--> RPi button, parallel with 10k pull-down to GND
+
+////////////////////
+
+
+
+Dan Bogachek
+09/28/2015
+
+'''
+
+
+
 import time
 import RPi.GPIO as GPIO
 import sys
@@ -71,5 +153,4 @@ while 1:
 	if GPIO.input(button):
 		advance(5, 40, run_times)
 		run_times = run_times + 1	
-
 GPIO.cleanup()

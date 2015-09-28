f72ea15439e70c9c79b586655fa8f45246a13a3d
bogasaur <bogachek.daniel@gmail.com>
2015-09-28 23:06:15 +0000

    Changed directory formatting to proper padding with zeros for sequential listing in Unix directories

--- scan.py
+++ scan.py
@@ -1,87 +1,5 @@
 #!/usr/bin/env python
 
-'''
-
-This code is for the TW Pi Scanner.
-
-Given pulses of .01 seconds, this stepper motor has 200 steps, hence 40 steps advancement * 5 = full revolution.
-
-Upon the user's press of the input button, the stage rotates and takes a picture for a total of 5 times
-to complete a single revolution. The 5 images are saved in the directory provided and are named after said
-directory.
-
-GPIO 23 (step) on the Pi is the step control on the A9488 stepper driver
-GPIO 18 (dir) on the Pi is the direction control on the A9488 stepper driver
-GPIO 24 (button) on the Pi is the tactile switch input
-
-
-Hardware Connections
-
-
-
-//////////////////// 5V Regulator
-
-VIN <--> VCC (12V and abvove)
-GND <--> GND
-VOUT <--> RPi 5V
-
-////////////////////
-
-
-
-
-//////////////////// RPi
-
-step <--> A9488 STEP
-dir <--> A9488 DIR
-button <--> tactile switch output, parallel with 10K pull-down to GND
-GND <--> GND
-5V <--> VOUT
-3.3V <--> A9488 VDD
-
-////////////////////
-
-
-
-
-//////////////////// A9488 Stepper Driver
-
-IMPORTANT FOR HARDWARE SETUP: Current Limit = VREF × 2.5
-Value set at .4V so that current limit is 1A
--
-VMOT <--> VCC
-GND <--> GND
-VDD <--> VDD 3.3V from RPi - RPi IO is 3.3V and VDD on the stepper driver uses VDD as logic reference
-GND <--> GND
--
-Connect RST & SLP
-STEP <--> RPi step
-DIR <--> RPi dir
--
-1A,1B - first stepper coil
-2A,2B - second stepper coil
-
-////////////////////
-
-
-
-
-//////////////////// Tactile Switch
-
-tactile switch input <--> 3.3V
-tactile switch output <--> RPi button, parallel with 10k pull-down to GND
-
-////////////////////
-
-
-
-Dan Bogachek
-09/28/2015
-
-'''
-
-
-
 import time
 import RPi.GPIO as GPIO
 import sys
@@ -124,10 +42,15 @@ def counter(cycles):
 
 def advance(reps, steps, run_times):
 	factor = run_times*5;
-	for i in range(0+factor, reps+factor):
+	for i in range(factor, reps+factor):
 		clockwise(steps)
 		time.sleep(.5)
-		camera.capture('blah%d.jpg' % i)
+
+		if(i<10): camera.capture('img000%d.jpg' % i)
+		elif(i<100): camera.capture('img00%d.jpg' % i)
+		elif(i<1000): camera.capture('img0%d.jpg' % i)
+		else: camera.capture('img%d.jpg' % i)
+
 		time.sleep(.5)
 
 flag = 0
@@ -153,4 +76,5 @@ while 1:
 	if GPIO.input(button):
 		advance(5, 40, run_times)
 		run_times = run_times + 1	
+
 GPIO.cleanup()
